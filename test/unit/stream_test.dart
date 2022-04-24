888560d4dfb5c2682a8b9a82b2798dce3424c253
Yakov Karpov <yakovkarpoff@gmail.com>
2022-04-25 01:43:19 +0300

    Add tests

--- test/unit/stream_test.dart
+++ test/unit/stream_test.dart
@@ -3,6 +3,18 @@ import 'dart:async';
 import 'package:batteries/batteries.dart';
 import 'package:test/test.dart';
 
+abstract class A {
+  final String value;
+
+  const A(this.value);
+}
+
+mixin X {}
+
+class B = A with X;
+
+class C = A with X;
+
 void main() => group('stream', () {
       test('relieve', () {
         const duration = Duration(microseconds: 100);
@@ -46,6 +58,123 @@ void main() => group('stream', () {
             for (var i = 0; i < 1000; i++) i,
           ]).map<bool>(isPrime).asBroadcastStream().relieve(duration),
           emitsInOrder(firstThousand),
+        );
+      });
+
+      group('transformOnType', () {
+        String sum(String a, String b) => a + b;
+        test(
+          'Untouched non-broadcast stream',
+          () => expectLater(
+            Stream<A>.fromIterable(const [B('a'), B('a'), B('a')])
+                .transformOnType<C>(
+                  (cs) => cs.map((c) => c.value.toUpperCase()).map(C.new),
+                )
+                .map((event) => event.value)
+                .reduce(sum),
+            completion('aaa'),
+          ),
+        );
+        test(
+          'Untouched broadcast stream',
+          () => expectLater(
+            Stream<A>.fromIterable(const [B('a'), B('a'), B('a')])
+                .asBroadcastStream()
+                .transformOnType<C>(
+                  (cs) => cs.map((c) => c.value.toUpperCase()).map(C.new),
+                )
+                .map((event) => event.value)
+                .reduce(sum),
+            completion('aaa'),
+          ),
+        );
+        test(
+          'Transformed non-broadcast stream',
+          () => expectLater(
+            Stream<A>.fromIterable(const [B('a'), C('A'), B('a')])
+                .transformOnType<C>(
+                  (cs) => cs.map((c) => c.value.toUpperCase()).map(C.new),
+                )
+                .map((event) => event.value)
+                .reduce(sum),
+            completion('aAa'),
+          ),
+        );
+        test(
+          'Transformed broadcast stream',
+          () => expectLater(
+            Stream<A>.fromIterable(const [B('a'), C('A'), B('a')])
+                .asBroadcastStream()
+                .transformOnType<C>(
+                  (cs) => cs.map((c) => c.value.toUpperCase()).map(C.new),
+                )
+                .map((event) => event.value)
+                .reduce(sum),
+            completion('aAa'),
+          ),
+        );
+        test(
+          'Transformer retains inertia stream contract on non-broadcast stream',
+          () async {
+            StreamSubscription<A>? sub;
+            var transformedHasEmitted = false;
+            final controller = StreamController<A>();
+            final transformed = controller.stream
+                .transformOnType<C>((selected) => selected)
+                .map((event) {
+              transformedHasEmitted = true;
+              return event;
+            });
+
+            Future<void> nextEventLoop() => Future<void>.delayed(Duration.zero);
+            void emit() => controller.add(const C('a'));
+            void subscribe() => sub = transformed.listen((event) {});
+
+            await nextEventLoop();
+            expect(transformedHasEmitted, false);
+            emit();
+            await nextEventLoop();
+            expect(transformedHasEmitted, false);
+            subscribe();
+            await nextEventLoop();
+            expect(transformedHasEmitted, true);
+
+            await sub?.cancel();
+            await controller.close();
+          },
+        );
+        test(
+          'Transformer retains inertia stream contract on broadcast stream',
+          () async {
+            StreamSubscription<A>? sub;
+            var transformedHasEmitted = false;
+            final controller = StreamController<A>.broadcast();
+            final transformed = controller.stream
+                .transformOnType<C>((selected) => selected)
+                .map((event) {
+              transformedHasEmitted = true;
+              return event;
+            });
+
+            Future<void> nextEventLoop() => Future<void>.delayed(Duration.zero);
+            void emit() => controller.add(const C('a'));
+            void subscribe() => sub = transformed.listen((event) {});
+
+            await nextEventLoop();
+            expect(transformedHasEmitted, false);
+            emit();
+            await nextEventLoop();
+            expect(transformedHasEmitted, false);
+            subscribe();
+            await nextEventLoop();
+            expect(transformedHasEmitted, false);
+            emit();
+            await nextEventLoop();
+            expect(transformedHasEmitted, true);
+
+            await sub?.cancel();
+            await controller.close();
+          },
         );
       });
     });
